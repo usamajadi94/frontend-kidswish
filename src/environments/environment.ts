0bec5fcb988f4d98842bd84071f9e56a7e9d953a
usamajadi94 <usamajadi@gmail.com>
2026-03-06 03:54:44 +0500

    Replicate base code

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,11 @@
 import { IEnvironmentalVariables } from '../app/EnvironmentalVariables';
 export const environment: IEnvironmentalVariables = {
     production: false,
-    // apiRoot: 'http://cloudfusion-001-site10.qtempurl.com/',
+    //apiRoot: 'http://cloudfusion-001-site10.qtempurl.com/',
     // apiRoot: "https://api.dabossleaf.com/",
 
     apiRoot: 'https://localhost:44327/',
-    // apiRoot: 'http://cloudfusion-001-site10.qtempurl.com/',
-    // apiRoot: 'http://cloudfusion-001-site10.qtempurl.com/',
+
     env: 'local',
     site: {
         baseUrl: '',
